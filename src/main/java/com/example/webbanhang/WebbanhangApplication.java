171931c470e2bca8360c0e115d861aa945e54506
EsercitoHaki <esercitohaki2003@gmail.com>
2024-09-25 22:39:30 +0700

    first commit

---
+++ src/main/java/com/example/webbanhang/WebbanhangApplication.java
@@ -0,0 +1,13 @@
+package com.example.webbanhang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebbanhangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebbanhangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/webbanhang/WebbanhangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webbanhang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebbanhangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
