b7cd88219d896275a7936a129add2bd676c89b6c
EsercitoHaki <esercitohaki2003@gmail.com>
2024-09-27 02:06:46 +0700

    ModelMapper Configurations

---
+++ src/main/java/com/example/webbanhang/configurations/MapperConfigurations.java
@@ -0,0 +1,14 @@
+package com.example.webbanhang.configurations;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MapperConfigurations {
+    @Bean
+    public ModelMapper modelMapper()
+    {
+        return new ModelMapper();
+    }
+}
