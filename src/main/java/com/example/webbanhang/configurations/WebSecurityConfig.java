0272468430e65ebcd36277416e60e9562f1ad5fb
EsercitoHaki <esercitohaki2003@gmail.com>
2024-11-30 14:10:02 +0700

    fix lỗi đăng ký tài khoản

--- src/main/java/com/example/webbanhang/configurations/WebSecurityConfig.java
+++ src/main/java/com/example/webbanhang/configurations/WebSecurityConfig.java
@@ -6,8 +6,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -18,7 +20,9 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import static org.springframework.http.HttpMethod.*;
 
 @Configuration
-@EnableMethodSecurity
+//@EnableMethodSecurity
+@EnableWebSecurity(debug = true)
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 @EnableWebMvc
 @RequiredArgsConstructor
 public class WebSecurityConfig {
