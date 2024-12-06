e38f3b76f0286fee04f3d2b20a17e1b08d1bc5b2
EsercitoHaki <esercitohaki2003@gmail.com>
2024-12-07 01:32:27 +0700

    đăng nhập bằng gg

---
+++ src/main/java/com/example/webbanhang/components/GoogleTokenUtils.java
@@ -0,0 +1,26 @@
+package com.example.webbanhang.components;
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
+
+@Component
+public class GoogleTokenUtils {
+    private static final String CLIENT_ID = "YOUR_FACEBOOK_APP_ID";
+
+    public GoogleIdToken verifyToken(String idTokenString) throws Exception {
+        GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), new JacksonFactory())
+                .setAudience(Collections.singletonList(CLIENT_ID))
+                .build();
+
+        return verifier.verify(idTokenString);
+    }
+}
+
--- src/main/java/com/example/webbanhang/services/IUserService.java
+++ src/main/java/com/example/webbanhang/services/IUserService.java
@@ -6,6 +6,7 @@ import com.example.webbanhang.exceptions.DataNotFoundException;
 import com.example.webbanhang.models.User;
 
 public interface IUserService {
+    boolean existsByGoogleAccountId(int googleAccountId);
     User createUser(UserDTO userDTO) throws Exception;
     String login(String phoneNumber, String password, Long roleId) throws Exception;
     User getUserDetailsFromToken(String token) throws Exception;
