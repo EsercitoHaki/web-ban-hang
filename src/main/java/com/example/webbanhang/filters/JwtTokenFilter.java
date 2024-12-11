32114c76fae5ef1ad7af07dd4c2fbf95c6fe15f4
EsercitoHaki <esercitohaki2003@gmail.com>
2024-12-11 15:59:53 +0700

    fix lỗi lấy ra tất cả order admin

--- src/main/java/com/example/webbanhang/filters/JwtTokenFilter.java
+++ src/main/java/com/example/webbanhang/filters/JwtTokenFilter.java
@@ -71,7 +71,7 @@ public class JwtTokenFilter extends OncePerRequestFilter {
                 Pair.of(String.format("%s/healthcheck/health", apiPrefix), "GET"),
                 Pair.of(String.format("%s/roles", apiPrefix), "GET"),
                 Pair.of(String.format("%s/order/**", apiPrefix), "GET"),
-                Pair.of(String.format("%s/orders/get-orders-by-keyword", apiPrefix), "GET"),
+
                 Pair.of(String.format("%s/products", apiPrefix), "GET"),
                 Pair.of(String.format("%s/products/recommendations", apiPrefix), "GET"),
                 Pair.of(String.format("%s/categories", apiPrefix), "GET"),
